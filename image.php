9c7592c40f2807f990c8e26cb9198056fe859540
Isabel Castillo <me@isabelcastillo.com>
2016-12-23 18:04:47 -0500

    Update name

--- image.php
+++ image.php
@@ -2,29 +2,29 @@
 if ( isset( $_GET['zpl'] ) ) {
 	$longitudes_raw = unserialize( $_GET['zpl'] );
 	for ( $n = 0; $n <= 16; $n++ ) {
-		$longitudes[] = zpcd_sanitize_data( $longitudes_raw[ $n ] );
+		$longitudes[] = zpci_sanitize_data( $longitudes_raw[ $n ] );
 	}
 }
 if ( isset( $_GET['zpc'] ) ) {
 	$cusps_raw = unserialize( $_GET['zpc'] );
 	for ( $c = 1; $c <= 12; $c++ ) {
-		$cusps[ $c ] = zpcd_sanitize_data( $cusps_raw[ $c ] );
+		$cusps[ $c ] = zpci_sanitize_data( $cusps_raw[ $c ] );
 	}
 }
 if ( isset( $_GET['zps'] ) ) {
 	$speed_raw = unserialize( $_GET['zps'] );
 	for ( $s = 0; $s <= 12; $s++ ) {
-		$speed[ $s ] = zpcd_sanitize_data( $speed_raw[ $s ] );
+		$speed[ $s ] = zpci_sanitize_data( $speed_raw[ $s ] );
 	}
 }
 if ( isset( $_GET['zpu'] ) ) {
-	$unknown_time = zpcd_sanitize_data( unserialize( $_GET['zpu'] ) );
+	$unknown_time = zpci_sanitize_data( unserialize( $_GET['zpu'] ) );
 }
 if ( isset( $_GET['zpi'] ) ) {
 	$i18n_raw = unserialize( $_GET['zpi'] );
 
 	foreach ( $i18n_raw as $key => $str ) {
-		$i18n[ $key ] = zpcd_sanitize_data( $str );
+		$i18n[ $key ] = zpci_sanitize_data( $str );
 	}
 }
 
@@ -33,7 +33,7 @@ if ( isset( $_GET['zpcustom'] ) ) {
 	$customizer_raw = unserialize( $_GET['zpcustom'] );
 
 	foreach ( $customizer_raw as $key => $value ) {
-		$customizer[ $key ] = zpcd_sanitize_data( $value );
+		$customizer[ $key ] = zpci_sanitize_data( $value );
 	}
 }
 
@@ -64,7 +64,7 @@ $off_white = imagecolorallocate($im,248,248,248);
 // @todo merge this into the above as we're getting customizer values, convert them there.
 if ( ! empty( $customizer ) ) {
 	foreach ( $customizer as $k => $hex ) {
-		$customizer_rgb[ $k ] = zpcd_hex2rgb( $hex );
+		$customizer_rgb[ $k ] = zpci_hex2rgb( $hex );
 	}
 }
 
@@ -196,20 +196,20 @@ imagefilledrectangle( $im, 0, 0, $overall_size, $overall_size, $white );
 imagefilledellipse( $im, $center_pt, $center_pt, $overall_size - 1, $overall_size - 1, $outer_bg_color );// make it smaller to fit antialiased border
 
 // antialias border to overall outer
-zpcd_antialiased_ellipse($im, $center_pt, $center_pt, $center_pt - 0.5, $center_pt - 0.5, $outer_bg_color);
+zpci_antialiased_ellipse($im, $center_pt, $center_pt, $center_pt - 0.5, $center_pt - 0.5, $outer_bg_color);
 
 // draw the signs circle of the chartwheel
 imagefilledellipse($im, $center_pt, $center_pt, $outer_diameter, $outer_diameter, $signs_wheel_color);
 
-zpcd_antialiased_ellipse($im, $center_pt, $center_pt, $radius_outer, $radius_outer, $signs_border_color);
+zpci_antialiased_ellipse($im, $center_pt, $center_pt, $radius_outer, $radius_outer, $signs_border_color);
 
 // draw the houses circle of the chartwheel
 imagefilledellipse($im, $center_pt, $center_pt, $diameter, $diameter, $wheel_bg_color);
-zpcd_antialiased_ellipse($im, $center_pt, $center_pt, $radius, $radius, $houses_border_color);
+zpci_antialiased_ellipse($im, $center_pt, $center_pt, $radius, $radius, $houses_border_color);
 
 // draw the inner circle of the chartwheel
 imagefilledellipse($im, $center_pt, $center_pt, $diameter - ($inner_diameter_offset * 2), $diameter - ($inner_diameter_offset * 2), $inner_wheel_color);
-zpcd_antialiased_ellipse($im, $center_pt, $center_pt, $radius_inner, $radius_inner, $inner_wheel_border_color);
+zpci_antialiased_ellipse($im, $center_pt, $center_pt, $radius_inner, $radius_inner, $inner_wheel_border_color);
 
 // ------------------------------------------
 
@@ -223,7 +223,7 @@ for ($i = $offset_from_start_of_sign; $i <= $offset_from_start_of_sign + 330; $i
 	$x2 = -($radius + $outer_diameter_distance) * cos(deg2rad($i));
 	$y2 = -($radius + $outer_diameter_distance) * sin(deg2rad($i));
 
-	zpcd_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $signs_divider_color );
+	zpci_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $signs_divider_color );
 
 }
 
@@ -241,10 +241,10 @@ if ( empty( $unknown_time ) ) {
 		$x2 = -( $radius - $inner_diameter_offset ) * cos( deg2rad( $angle ) );
 		$y2 = -( $radius - $inner_diameter_offset ) * sin( deg2rad( $angle ) );
 
-		zpcd_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $houses_divider_color );
+		zpci_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $houses_divider_color );
 
 		// display the house cusp numbers
-		zpcd_house_number_coordinates( $i, -$angle, $radius - $inner_diameter_offset, $xy );
+		zpci_house_number_coordinates( $i, -$angle, $radius - $inner_diameter_offset, $xy );
 		imagettftext( $im, 10, 0, $xy[0] + $center_pt, $xy[1] + $center_pt, $house_number_color, $dir . 'HamburgSymbols.ttf', $i );
 	}
 
@@ -256,7 +256,7 @@ if ( empty( $unknown_time ) ) {
 	$y1 = -($radius - 10 ) * sin( deg2rad( $angle ) );
 	$x2 = -( $radius - $inner_diameter_offset + 1 ) * cos( deg2rad( $angle ) );
 	$y2 = -( $radius - $inner_diameter_offset + 1 ) * sin( deg2rad( $angle ) );
-	zpcd_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
+	zpci_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
 
 	//draw the arrow for the Ascendant
 	$dist_asc_first_house = ( $angle < 0 ) ? ( $angle + 360 ) : $angle;
@@ -265,10 +265,10 @@ if ( empty( $unknown_time ) ) {
 	$angle2 = 65 + $valuea;
 	$x2 = $x1 + (20 * cos(deg2rad($angle1)));
 	$y2 = $y1 + (20 * sin(deg2rad($angle1)));
-	zpcd_draw_thick_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color );
+	zpci_draw_thick_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color );
 	$x2 = $x1 - (20 * cos(deg2rad($angle2)));
 	$y2 = $y1 + (20 * sin(deg2rad($angle2)));
-	zpcd_draw_thick_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color );
+	zpci_draw_thick_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color );
 
 	// draw the thick line for the Descendant
 	$x1 = -( $radius - $inner_diameter_offset + 1 ) * cos( deg2rad( $angle - 180 ) );
@@ -277,7 +277,7 @@ if ( empty( $unknown_time ) ) {
 	$x2 = -( $radius - 2 ) * cos( deg2rad( $angle - 180 ) );
 	$y2 = -( $radius - 2 ) * sin( deg2rad( $angle - 180 ) );
 
-	zpcd_draw_thick_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color );
+	zpci_draw_thick_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color );
 
 	// ------------------------------------------
 
@@ -294,15 +294,15 @@ if ( empty( $unknown_time ) ) {
 	$y1 = -($radius - $inner_diameter_offset + 1) * sin(deg2rad($angle));
 	$x2 = -($radius - 10) * cos(deg2rad($angle));
 	$y2 = -($radius - 10) * sin(deg2rad($angle));
-	zpcd_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
+	zpci_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
 
 	// draw the arrow for the MC
 	$x1 = $x2 + (20 * cos(deg2rad($angle1)));
 	$y1 = $y2 + (20 * sin(deg2rad($angle1)));
-	zpcd_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
+	zpci_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
 	$x1 = $x2 - (20 * cos(deg2rad($angle2)));
 	$y1 = $y2 + (20 * sin(deg2rad($angle2)));
-	zpcd_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
+	zpci_draw_thick_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $angles_arrow_color);
 }
 
 // ------------------------------------------
@@ -324,7 +324,7 @@ for ($i = $offset_from_start_of_sign; $i <= $offset_from_start_of_sign + 359; $i
 	}
 
 	$cnt = $cnt + 1;
-	zpcd_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $houses_divider_color );
+	zpci_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $houses_divider_color );
 }
 
 // ------------------------------------------
@@ -356,7 +356,7 @@ for ( $i = 1; $i <= 12; $i++ ) {
 		$clr_to_use = $water_sign_color;
 	}
 
-	zpcd_draw_bold_text($im, 16, 0, $x1 + $center_pt, $y1 + $center_pt, $clr_to_use, $dir . 'HamburgSymbols.ttf', chr( $sign_glyph[ $i ] ), 1 );
+	zpci_draw_bold_text($im, 16, 0, $x1 + $center_pt, $y1 + $center_pt, $clr_to_use, $dir . 'HamburgSymbols.ttf', chr( $sign_glyph[ $i ] ), 1 );
 
 }
 
@@ -365,7 +365,7 @@ for ( $i = 1; $i <= 12; $i++ ) {
 // put planets in chartwheel
 	
 // sort longitudes in descending order from 360 down to 0
-zpcd_sort_planets_by_descending_longitude( $num_planets, $longitudes, $sort, $sort_pos );
+zpci_sort_planets_by_descending_longitude( $num_planets, $longitudes, $sort, $sort_pos );
 
 $flag = false;
 
@@ -401,11 +401,11 @@ for ($i = $num_planets - 1; $i >= 0; $i--) {
 			$x2 = (-$radius + 6) * cos($angle_to_use);
 			$y2 = ($radius - 6) * sin($angle_to_use);
 		}
-		zpcd_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $degree_color );
+		zpci_image_smooth_line( $im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $degree_color );
 		$flag = ! $flag;
 	}
 
-	zpcd_planet_glyph_coordinates( $angle_to_use, $glyph_radius, $i, $sort_pos[ $i ], $xy, $sort );
+	zpci_planet_glyph_coordinates( $angle_to_use, $glyph_radius, $i, $sort_pos[ $i ], $xy, $sort );
 
 	// Use text instead of glyph for Vertex
 	if ( 14 == $sort_pos[ $i ] ) {
@@ -420,7 +420,7 @@ for ($i = $num_planets - 1; $i >= 0; $i--) {
 
 	// Display degrees
 	// Use this again to get coordinates to display the degrees
-	zpcd_planet_glyph_coordinates( $angle_to_use, $dms_radius, $i, $sort_pos[ $i ], $xy_dms, $sort, 9 );
+	zpci_planet_glyph_coordinates( $angle_to_use, $dms_radius, $i, $sort_pos[ $i ], $xy_dms, $sort, 9 );
 	$sign_num = floor( $longitudes[ $sort_pos[ $i ] ] / 30 );
 	$pos_in_sign = $longitudes[ $sort_pos[ $i ] ] - ( $sign_num * 30 );
 	$degrees = floor( $pos_in_sign );
@@ -517,7 +517,7 @@ for ($i = $num_planets - 1; $i >= 0; $i--) {
 						imagesetstyle($im, $style);
 						imageline($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, IMG_COLOR_STYLED);	
 					} else {
-						zpcd_image_smooth_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $aspect_color);	
+						zpci_image_smooth_line($im, $x1 + $center_pt, $y1 + $center_pt, $x2 + $center_pt, $y2 + $center_pt, $aspect_color);	
 					}
 
 					
@@ -558,14 +558,14 @@ if ( ! empty( $unknown_time ) ) {
 /**
  * Strip all tags and whitespace
  */
-function zpcd_sanitize_data( $data ) {
+function zpci_sanitize_data( $data ) {
 	$data = preg_replace( '@<(script|style)[^>]*?>.*?</\\1>@si', '', $data );
 	$data = strip_tags($data);
 	$data = preg_replace('/[\r\n\t ]+/', ' ', $data);
  	return trim( $data );
 }
 
-function zpcd_sort_planets_by_descending_longitude($num_planets, $longitude, &$sort, &$sort_pos) {
+function zpci_sort_planets_by_descending_longitude($num_planets, $longitude, &$sort, &$sort_pos) {
 // load all $longitude() into sort() and keep track of the planet numbers in $sort_pos()
 	for ($i = 0; $i <= $num_planets - 1; $i++)
 	{
@@ -596,7 +596,7 @@ function zpcd_sort_planets_by_descending_longitude($num_planets, $longitude, &$s
 /**
  * Check if 2 planet longitudes are too close together
  */
-function zpcd_is_planet_too_close( $long_1, $long_2 ) {
+function zpci_is_planet_too_close( $long_1, $long_2 ) {
 	$distance = $long_1 - $long_2;
 
 	if ( $distance < 0 ) {
@@ -610,7 +610,7 @@ function zpcd_is_planet_too_close( $long_1, $long_2 ) {
 /**
  * Calculate the coordinates on the wheel where a planet glyph should be displayed
  */
-function zpcd_planet_glyph_coordinates($angle_to_use, $radii, $i, $sort_pos, &$xy, $sorted_longitudes = false, $glyph_height = 16) {
+function zpci_planet_glyph_coordinates($angle_to_use, $radii, $i, $sort_pos, &$xy, $sorted_longitudes = false, $glyph_height = 16) {
 	$glyph_width = 16;
 	$gap_pl_glyph = -10;
 	// take into account the width and height of the glyph, defined below
@@ -624,21 +624,21 @@ function zpcd_planet_glyph_coordinates($angle_to_use, $radii, $i, $sort_pos, &$x
 	// If longitude is within 3 degrees of previous, move it over
 	// iF there is a previous one to compare it to
 	if ( isset( $sorted_longitudes[ $i + 1 ] ) ) {
-		if ( zpcd_is_planet_too_close( $sorted_longitudes[ $i ], $sorted_longitudes[ $i + 1 ] ) ) {
+		if ( zpci_is_planet_too_close( $sorted_longitudes[ $i ], $sorted_longitudes[ $i + 1 ] ) ) {
 
 			// Yes, too close
 			$angle_to_use += deg2rad(3);
 			
 			// Was the previous also too close to its previous planet?
 			if ( isset( $sorted_longitudes[ $i + 2 ] ) ) {
-				if ( zpcd_is_planet_too_close( $sorted_longitudes[ $i + 1 ], $sorted_longitudes[ $i + 2 ] ) ) {
+				if ( zpci_is_planet_too_close( $sorted_longitudes[ $i + 1 ], $sorted_longitudes[ $i + 2 ] ) ) {
 
 					// Yes, too close
 					$angle_to_use += deg2rad(6);
 			
 					// was the previous-previous also too close to its previous planet?
 					if ( isset( $sorted_longitudes[ $i + 3 ] ) ) {
-						if ( zpcd_is_planet_too_close( $sorted_longitudes[ $i + 2 ], $sorted_longitudes[ $i + 3 ] ) ) {
+						if ( zpci_is_planet_too_close( $sorted_longitudes[ $i + 2 ], $sorted_longitudes[ $i + 3 ] ) ) {
 							$angle_to_use += deg2rad(9);
 						}
 					}
@@ -667,7 +667,7 @@ function zpcd_planet_glyph_coordinates($angle_to_use, $radii, $i, $sort_pos, &$x
 	return ($xy);
 }
 
-function zpcd_house_number_coordinates( $num, $angle, $radii, &$xy ) {
+function zpci_house_number_coordinates( $num, $angle, $radii, &$xy ) {
 	$char_width = ($num < 10) ? 10 : 16;
 	$half_char_width = $char_width / 2;
 	$char_height = 12;
@@ -743,7 +743,7 @@ function zpcd_house_number_coordinates( $num, $angle, $radii, &$xy ) {
 	$xy[1] = $ypos0 + $y_adj + ( $radii * sin( deg2rad( $angle ) ) );
 	return ($xy);
 }
-function zpcd_draw_bold_text($image, $size, $angle, $x_cord, $y_cord, $clr_to_use, $fontfile, $text, $boldness) {
+function zpci_draw_bold_text($image, $size, $angle, $x_cord, $y_cord, $clr_to_use, $fontfile, $text, $boldness) {
 	$_x = array(1, 0, 1, 0, -1, -1, 1, 0, -1);
 	$_y = array(0, -1, -1, 0, 0, -1, 1, 1, 1);
 
@@ -751,7 +751,7 @@ function zpcd_draw_bold_text($image, $size, $angle, $x_cord, $y_cord, $clr_to_us
 		ImageTTFText( $image, $size, $angle, $x_cord+$_x[$n], $y_cord+$_y[$n], $clr_to_use, $fontfile, $text );
 	}
 }
-function zpcd_draw_thick_line( $image, $x1, $y1, $x2, $y2, $color, $boldness = 3 ) {
+function zpci_draw_thick_line( $image, $x1, $y1, $x2, $y2, $color, $boldness = 3 ) {
 	$center = round($boldness/2);
 	for($i=0;$i<$boldness;$i++) {
 		$a = $center-$i;
@@ -763,12 +763,12 @@ function zpcd_draw_thick_line( $image, $x1, $y1, $x2, $y2, $color, $boldness = 3
 			if($b<0){$b -= $b;}
 			$c = sqrt($a*$a + $b*$b);
 			if($c<=$boldness) {
-				zpcd_image_smooth_line($image, $x1 +$i, $y1+$j, $x2 +$i, $y2+$j, $color);
+				zpci_image_smooth_line($image, $x1 +$i, $y1+$j, $x2 +$i, $y2+$j, $color);
 			}
 		}
 	}
 } 
-function zpcd_image_smooth_line( $image, $x1, $y1, $x2, $y2, $color ) {
+function zpci_image_smooth_line( $image, $x1, $y1, $x2, $y2, $color ) {
 	$colors = imagecolorsforindex ( $image , $color );
 	if ( $x1 == $x2 ) {
 	imageline ( $image , $x1 , $y1 , $x2 , $y2 , $color ); // Vertical line
@@ -820,7 +820,7 @@ function zpcd_image_smooth_line( $image, $x1, $y1, $x2, $y2, $color ) {
 	}
 	}
 }
-function zpcd_setpixel4($img, $cx, $cy, $deltax, $deltay, $color) {
+function zpci_setpixel4($img, $cx, $cy, $deltax, $deltay, $color) {
 	imagesetpixel($img, $cx + $deltax, $cy + $deltay, $color);
 	imagesetpixel($img, $cx - $deltax, $cy + $deltay, $color);
 	imagesetpixel($img, $cx + $deltax, $cy - $deltay, $color);
@@ -829,7 +829,7 @@ function zpcd_setpixel4($img, $cx, $cy, $deltax, $deltay, $color) {
 /**
  * Ellipse with antialiasing - based on Wu's algorithm
  */
-function zpcd_antialiased_ellipse( $img, $cx, $cy, $radius_x, $radius_y, $color ) {
+function zpci_antialiased_ellipse( $img, $cx, $cy, $radius_x, $radius_y, $color ) {
 	$radius_x2 = $radius_x * $radius_x;
 	$radius_y2 = $radius_y * $radius_y;
 	static $max_transparency = 0x7F; // 127
@@ -841,8 +841,8 @@ function zpcd_antialiased_ellipse( $img, $cx, $cy, $radius_x, $radius_y, $color 
 		$transparency = round($error * $max_transparency);
 		$alpha  = $color | ($transparency << 24);
 		$alpha2 = $color | (($max_transparency - $transparency) << 24);
-		zpcd_setpixel4($img, $cx, $cy, $x, floor($y),   $alpha);
-		zpcd_setpixel4($img, $cx, $cy, $x, floor($y)+1, $alpha2);
+		zpci_setpixel4($img, $cx, $cy, $x, floor($y),   $alpha);
+		zpci_setpixel4($img, $cx, $cy, $x, floor($y)+1, $alpha2);
 	}
 	// right and left halves
 	$quarter = round($radius_y2 / sqrt($radius_x2 + $radius_y2));
@@ -852,11 +852,11 @@ function zpcd_antialiased_ellipse( $img, $cx, $cy, $radius_x, $radius_y, $color 
 		$transparency = round($error * $max_transparency);
 		$alpha  = $color | ($transparency << 24);
 		$alpha2 = $color | (($max_transparency - $transparency) << 24);
-		zpcd_setpixel4($img, $cx, $cy, floor($x),   $y, $alpha);
-		zpcd_setpixel4($img, $cx, $cy, floor($x)+1, $y, $alpha2);
+		zpci_setpixel4($img, $cx, $cy, floor($x),   $y, $alpha);
+		zpci_setpixel4($img, $cx, $cy, floor($x)+1, $y, $alpha2);
 	}
 }
-function zpcd_hex2rgb( $hex ) {
+function zpci_hex2rgb( $hex ) {
 	$hex = ltrim( $hex, '#');
 	if(strlen($hex) == 3) {
 		$r = hexdec(substr($hex,0,1).substr($hex,0,1));
